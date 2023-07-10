c1ca84e3f5be9f80d59f92b9a7f9ee39a13f95c0
RopaDon <developer@insurehospitality.pro>
2023-07-10 14:42:42 -0500

    tweaked to run inna development context

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ const nextConfig = {
   eslint: {
       ignoreDuringBuilds: true,
   },
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
